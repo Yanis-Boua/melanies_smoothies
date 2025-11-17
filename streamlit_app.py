61a48ceae7cb3c75914b0dd8fdc5060d7af1996e
Yanis-Boua <69041608+Yanis-Boua@users.noreply.github.com>
2025-11-17 15:23:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,10 @@ st.write(
 title=st.text_input('Title', 'Entrez votre nom')
 st.write('The current title is', title)
 
-#cnx=st.connection("snowflake")
-#session=cnx.session()
+cnx=st.connection("snowflake")
+session=cnx.session()
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
